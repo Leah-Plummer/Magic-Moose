ba8bdfcb7de117b450a3eff2a251a33f348baa16
Leah Plummer <leahplumm@gmail.com>
2022-11-17 10:16:32 -0600

    moose replies to question with random answer generated from list

--- Program.cs
+++ Program.cs
@@ -5,65 +5,74 @@ Main();
 
 void Main()
 {
-    Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
+    Console.WriteLine("Welcome to the Magic Moose Simulator!");
     Console.WriteLine("--------------------------------------------");
     Console.WriteLine();
 
     // Let the moose speak!
-    MooseSays("H I, I'M  E N T H U S I A S T I C !");
+    MooseSays("H I, I'M  M A G I C !");
     MooseSays("Ask me a question!");
 
-    List<string> questions = new List<string>() 
-{
-    "Is Canada real?",
-    "Are you enthusiastic?", 
-    "Do you love C# yet?", 
-    "Do you want to know a secret?",
-};
-
-List<string> yesses = new List<string>() 
-{
-    "Really? It seems very unlikely.",
-    "Yay!", 
-    "Good job sucking up to your instructor!", 
-    "ME TOO!!!! I love secrets...tell me one!",
-};
-
-List<string> nos = new List<string>() 
-{
-    "I  K N E W  I T !!!",
-    "You should try it!", 
-    "You will...oh, yes, you will...", 
-    "Oh, no...secrets are the best, I love to share them!",
-};
-
-int i = 0; 
-
-foreach (string question in questions)
-{
-    Console.WriteLine($"{question}(Y/N): ");
     string answer = Console.ReadLine().ToLower();
 
-    while (answer != "y" && answer != "n")
+
+
+    List<string> responses = new List<string>() 
+{
+    "As I see it, yes.",
+    "Ask again later.",
+    "Better not tell you now.",
+    "Cannot predict now.",
+    "Concentrate and ask again.",
+    "Don't count on it.",
+    "It is certain.",
+    "It is decidedly so.",
+    "Most likely.",
+    "My reply is no.",
+    "My sources say no.",
+    "Outlook not so good.",
+    "Outlook good.",
+    "Reply hazy, try again.",
+    "Signs point to yes.",
+    "Very doubtful.",
+    "Without a doubt.",
+    "Yes.",
+    "Yes - definitely.",
+    "You may rely on it.",
+};
+
+
+
+
+
+   
+      Random i = new Random();
+      int genRand= i.Next(0,19);
+   
+
+// foreach (string response in responses)
+// {
+//     Console.WriteLine($"{response}(Y/N): ");
+
+    if (answer != "")
     {
-        Console.Write($"{question} (Y/N): ");
-        answer = Console.ReadLine().ToLower();
+        Console.Write($"{responses[genRand]}");
+        //answer = Console.ReadLine().ToLower();
     }
 
-     if (answer == "y") 
-    {
-        Console.WriteLine($"{yesses[i]}");
-        i++;
-    }
-    else
-    {
-        Console.WriteLine($"{nos[i]}") ;
-        i++;
-    }
+//      if (answer == "y") 
+//     {
+//         Console.WriteLine($"{yesses[i]}");
+//         i++;
+//     }
+//     else
+//     {
+//         Console.WriteLine($"{nos[i]}") ;
+//         i++;
+//     }
 
-}
-}
-
+// }
+ }
 
 void MooseSays(string message)
 {
@@ -96,13 +105,4 @@ void MooseSays(string message)
                        `^^` `^^^`
     ");
 };
-
-
-
-
-
-
-
-
-
 
